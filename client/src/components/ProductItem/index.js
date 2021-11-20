d1cc8e0b85fd144c4c49401bcb7a19f8b5509579
lverghese <loganverghese@gmail.com>
2021-11-20 15:35:54 -0500

    remove global store statement

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize, idbPromise } from "../../utils/helpers"
-import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import {useDispatch, useSelector} from 'react-redux';
 
